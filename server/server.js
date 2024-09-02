8602f3924de3f18f6a79fed75e095db87bd1c0f1
ScottGreenhalgh <scottgreenhalgh62@me.com>
2024-09-02 15:58:51 +0100

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import express, { response } from "express";
+const app = express();
+app.use(express.json());
+
+app.get("/", function (request, response) {
+  response.json("You are looking at my root route");
+});
+
+//start server
+app.listen(8080, function () {
+  console.log("Listening on port 8080");
+});
